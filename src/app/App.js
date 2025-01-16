126f9272304c5a05d579b9f0d479f33f25d3cb76
baberldn <berilbadan1903@gmail.com>
2025-01-16 13:47:59 +0300

    Update

--- src/app/App.js
+++ src/app/App.js
@@ -1,156 +1,81 @@
-import React from 'react';
-
 const App = () => {
   return (
-    <div className="flex items-center justify-start min-h-screen pl-10 bg-gray-100">
-      <main className="bg-[#FFDD55] w-[1000px] h-[700px] relative  ml-32">
-        <div className="bg-[#FFDD55] w-[300px] h-[700px] ml-56">
-   
-          <nav className="bg-black w-full h-[100px] absolute top-0 left-0 rounded-b-[60px] flex items-center justify-between pl-12 pr-8">
-            <img src="images/logo.png" alt="Logo" className="w-[50px] h-[50px] object-cover" />
-            <div className="absolute top-[35px] left-[420px] w-[674px] h-[34px] opacity-100 gap-[48px] flex items-center justify-between">
-              <ul className="flex space-x-[38px]">
-                <li className="text-white font-[Montserrat] text-[26px] font-semibold leading-[43.88px] text-left no-underline decoration-transparent">
-                  Home
-                </li>
-                <li className="text-white font-[Montserrat] text-[26px] font-semibold leading-[43.88px] text-left no-underline decoration-transparent">
-                  About
-                </li>
-                <li className="text-white font-[Montserrat] text-[26px] font-semibold leading-[43.88px] text-left no-underline decoration-transparent">
-                  Services
-                </li>
-                <li className="text-white font-[Montserrat] text-[26px] font-semibold leading-[43.88px] text-left no-underline decoration-transparent">
-                  Contact
-                </li>
-              </ul>
-            </div>
-          </nav>
-        </div>
-
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <main className="bg-[#FFDD55] w-full lg:w-[1000px] lg:h-[700px] relative p-4">
       
-        <div className="absolute top-[250px] left-[50px]">
-          <h1 className="text-black text-[54px] font-extrabold leading-[40px] text-left">
-            Hello,
-          </h1>
-        </div>
+        <nav className="bg-black w-full h-[120px] rounded-b-[40px] flex items-center justify-between px-4 lg:px-12 absolute top-0 left-0">
+          <img src="images/logo.png" alt="Logo" className="w-[50px] h-[50px]" />
+          <div className="hidden lg:flex gap-[48px]">
+            <ul className="flex space-x-[38px]">
+              <li className="text-white text-[26px] font-semibold">Home</li>
+              <li className="text-white text-[26px] font-semibold">About</li>
+              <li className="text-white text-[26px] font-semibold">Services</li>
+              <li className="text-white text-[26px] font-semibold">Contact</li>
+            </ul>
+          </div>
+          <div className="lg:hidden">
+            <img
+              src="images/Hamburger_MD.png"
+              alt="Menu"
+              className="w-[50px] h-[50px]"
+            />
+          </div>
+        </nav>
 
-        <div className="absolute top-[320px] left-[50px]">
-          <h1 className="text-black text-[54px] font-extrabold leading-[40px] text-left">
-            I'm
-          </h1>
-        </div>
+    
+        <div className="lg:flex lg:justify-between lg:items-center mt-[160px]"> 
+          <div className="lg:text-left text-left">
+            <h1 className="text-black text-[34px] lg:text-[54px] font-extrabold mt-12">
+              Hello,
+            </h1>
+            <h1 className="text-black text-[34px] lg:text-[54px] font-extrabold mt-2">
+              I'm
+            </h1>
+            <h3 className="text-[#474A57] text-[15px] lg:text-[20px] font-semibold mt-4">
+              UI/UX Designer, React Developer
+            </h3>
+            <div className="flex space-x-4 mt-8">
+              <button className="w-[100px] lg:w-[194px] h-[40px] lg:h-[63px] bg-black text-white text-[12px] lg:text-[20px] font-semibold rounded-lg">
+                Hire Me
+              </button>
+              <button className="w-[100px] lg:w-[194px] h-[40px] lg:h-[63px] bg-black text-white text-[12px] lg:text-[20px] font-semibold rounded-lg">
+                Download CV
+              </button>
+            </div>
+          </div>
 
-        <div className="absolute top-[390px] left-[50px]">
-          <h2 className="text-[#474A57] text-[20px] font-semibold leading-[35px] text-left">
-            UI/UX Designer, React Developer
-          </h2>
-        </div>
-
-        <div className="absolute top-[470px] left-[50px] w-[194px] h-[63px] rounded-[15px] bg-[#00C6AE] opacity-100">
-        </div>
-
-        <div className="absolute top-[470px] left-[260px] w-[194px] h-[63px] rounded-[15px] bg-[#00C6AE] opacity-100">
-        </div>
-
-        <div className="absolute top-[463px] left-[43px] w-[194px] h-[63px] rounded-[15px] bg-black opacity-100 flex items-center justify-center">
-          <span className="text-white font-[Montserrat] text-[20px] font-semibold">
-            HIRE ME
-          </span>
-        </div>
-
-        <div className="absolute top-[463px] left-[253px] w-[194px] h-[63px] rounded-[15px] bg-black opacity-100 flex items-center justify-center">
-          <span className="text-white font-[Montserrat] text-[20px] font-semibold">
-            Download CV
-          </span>
-        </div>
-
-        <div className="absolute top-[180px] left-[500px]">
-          <img
-            src="images/avatar.png" 
-            alt="Profile"
-            className="w-[370px] h-[480px] rounded-lg" 
-          />
-        </div>
-
-        <div className="absolute top-[315px] left-[900px] flex flex-col gap-[10px]">
-          <img src="images/Component 1.png" alt="Additional 1" className="w-[76px] h-[76px] object-cover rounded-lg" />
-          <img src="images/Component 2.png" alt="Additional 2" className="w-[76px] h-[76px] object-cover rounded-lg" />
-          <img src="images/Component 3.png" alt="Additional 3" className="w-[76px] h-[76px] object-cover rounded-lg" />
-          <img src="images/Component 4.png" alt="Additional 4" className="w-[76px] h-[76px] object-cover rounded-lg" />
+        
+          <div className="flex items-center lg:justify-end mt-12 lg:mt-0"> 
+            <img
+              src="images/avatar.png"
+              alt="Profile"
+              className="w-[205px] lg:w-[370px] h-[245px] lg:h-[480px] rounded-lg"
+            />
+            <div className="flex flex-col gap-2 mt-4 lg:mt-0 ml-4">
+              <img
+                src="images/Component1.png"
+                alt="Icon 1"
+                className="w-[40px] lg:w-[76px] h-[40px] lg:h-[76px] rounded-lg"
+              />
+              <img
+                src="images/Component2.png"
+                alt="Icon 2"
+                className="w-[40px] lg:w-[76px] h-[40px] lg:h-[76px] rounded-lg"
+              />
+              <img
+                src="images/Component3.png"
+                alt="Icon 3"
+                className="w-[40px] lg:w-[76px] h-[40px] lg:h-[76px] rounded-lg"
+              />
+              <img
+                src="images/Component4.png"
+                alt="Icon 4"
+                className="w-[40px] lg:w-[76px] h-[40px] lg:h-[76px] rounded-lg"
+              />
+            </div>
+          </div>
         </div>
       </main>
-
-
-
-
-
-      {/* mobil */}
-      <div className="bg-[#FFDD55] w-[300px] h-[700px] ml-56">
-  <div className="bg-black w-[300px] h-[100px] rounded-b-[40px] flex justify-between items-center px-4">
-    <img src="images/logo.png" alt="Description" className="w-[50px] h-[50px] ml-[20px]" />
-    <img src="images/Hamburger_MD.png" alt="Right Image" className="w-[50px] h-[50px]" />
-  </div>
-  
-
-  <h1 className="text-black text-[34px] font-extrabold leading-[40px] text-left mt-[90px] ml-[20px]">
-    Hello,
-  </h1>
-  <h1 className="text-black text-[34px] font-extrabold leading-[20px] text-left mt-[20px] ml-[20px]">
-    I'm
-  </h1>
-  <h3 className="text-[#474A57] text-[15px] font-semibold text-left mt-[20px] ml-[20px]">
-    UI/UX Designer
-  </h3>
-  <h3 className="text-[#474A57] text-[15px] font-semibold text-left ml-[20px]">
-    React Developer
-  </h3>
-
-  <div className="mt-[30px] flex justify-start ml-[20px]">
-  <img src="images/avatar.png" alt="Profile" className="w-[205px] h-[245px]" />
-</div>
-<div className="mr-[15px] flex flex-col items-end mt-[-180px]">
-  <img src="images/Component 1.png" alt="Image 1" className="w-[40px] h-[40px] mb-[5px]" />
-  <img src="images/Component 2.png" alt="Image 2" className="w-[40px] h-[40px] mb-[5px]" />
-  <img src="images/Component 3.png" alt="Image 3" className="w-[40px] h-[40px] mb-[5px]" />
-  <img src="images/Component 4.png" alt="Image 4" className="w-[40px] h-[40px] mb-[5px]" />
-</div>
-<div className="flex space-x-[30px] mt-[30px] ml-[10px] relative">
-  <div className="absolute bottom-[5px] left-[155px] bg-black w-[100px] h-[25px] rounded-[15px_15px_15px_15px]">
-  <span className="text-white font-[Montserrat] text-[12px] font-semibold ml-[8px]">Download CV</span>
-  </div>
-
-
-  <div className="bg-[#00C6AE] w-[100px] h-[25px] rounded-[15px_15px_15px_15px]">
-  </div>
-
-  
-  <div className="absolute bottom-[5px] left-[-5px]  bg-black w-[100px] h-[25px] rounded-[15px_15px_15px_15px]"> <span className="text-white font-[Montserrat] text-[15px] font-semibold ml-[17px]">Hire Me</span>
-  </div>
-
- 
-  <div className="bg-[#00C6AE] w-[100px] h-[25px] rounded-[15px_15px_15px_15px]">
-    
-  </div>
-</div>
-
-
-
-
-
-
-</div>
-
-
-
-
-
-
-
-
-
-
-
-
     </div>
   );
 };
